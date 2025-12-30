1b4e88d4c4da04192007b6f4d6585e37bd45684e
Talonmom <lil_chanti50@hotmail.com>
2025-12-30 13:40:31 -0700

    Added files from the old one
    Wow. Now it's time to do this!

---
+++ numberGoUp.js
@@ -0,0 +1,48 @@
+time = 0
+test = 0
+
+totalTime = 80120
+console.log("Total time: " + Math.floor(totalTime / 6000) + "m " + Math.ceil((totalTime / 100) % 60) + "s")
+
+function update() {
+  document.getElementById("number").innerHTML = timeToNumber(time)
+  time++
+  //if (Math.floor((1.001 + time / 5e6) ** time + 0.01 * time - 1) >= 1e6 && test == 0) {test=1, console.log(time)}
+}
+
+function timeToNumber(x) {
+  //0 < x < 1e15
+  if (x < 10888) {
+    return Math.floor((1.001 + x / 5e6) ** x + 0.01 * x - 1).toLocaleString()
+  }
+  //1e15 < x < 1e100
+  else if (x < 13166) {
+    return "10<sup>" + ((1 + x / 7e6) ** (x - 10803) + 0.0006 * x + 7.3).toFixed(1)
+  }
+  //1e100 < x < 1e1e6
+  else if (x < 16627) {
+    return "10<sup>" + (Math.floor((1 + x / 7e6) ** (x - 10803) + 0.0006 * x + 7.3)).toLocaleString()
+  }
+  //1e1e6 < x < 10^^9
+  else if (x < 52011) {
+    return "10<sup>" + timeToNumber(x * 1.2 - 13769)
+  }
+  //10^^9 < x < 10^^100
+  else if (x < 55512) {
+    return "10^^" + (1.002 ** (x - 52055) + 0.00008 * x + 3.85).toFixed(2)
+  }
+  //10^100 < x < 10^^10^^10^^10
+  else if (x < 80120) {
+    return "10^^" + timeToNumber((x - 53690) ** 1.1)
+  }
+  //10^^10^^10^^10 < x
+  else {
+    return "10^^10^^10^^10"
+  }
+}
+
+setInterval(update, 10)
+
+function minsToTime(x,y) {
+  time = x * 6000 + y * 100
+}
