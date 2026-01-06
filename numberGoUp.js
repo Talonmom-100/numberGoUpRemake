e40764c5331865ec53458bca4781613bcd468d2c
Talonmom-100 <lil_chanti50@hotmail.com>
2026-01-06 14:00:40 -0700

    The moments
    All life begins and ends with an LNGI.
    
    I believe it to be true.
    
    (joke)

--- numberGoUp.js
+++ numberGoUp.js
@@ -2,7 +2,7 @@ time = 0
 test = 0
 timeup = 1
 
-totalTime = 130280
+totalTime = 130279
 document.getElementById("totaltime").innerHTML = "Total time: " + Math.floor(totalTime / 6000) + "m " + Math.ceil((totalTime / 100) % 60) + "s"
 
 function update() {
@@ -44,7 +44,7 @@ function timeToNumber(x) {
     return "10^^^" + timeToNumber((x - 88671) ** 1.13)
   }
   //10^^^^5 < x < 10^^^^100
-  else if (x < 130280) {
+  else if (x < 130279) {
     return "10^^^^" + (1.002 ** (x - 128000) + 0.00004 * x + 0.219).toFixed(3)
   }
   else {
