3989fea3375d346b50c02a1f8759ca0a6f4ab7bf
Talonmom-100 <lil_chanti50@hotmail.com>
2026-01-08 15:20:38 -0700

    It goes off the screen.
    Am I just doing something that doesn't need to be done?

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # numberGoUpRemake
 A remake of Demonin's Number Go Up. I didn't think it was broken... but I decided to remake it. All files are from the old one, because I could not find a repo on Github.
 Credit to Demonin for making the old one, though. (Hopefully I don't get banned)
+
+## Warning:
+At some later parts, you'll start seeing the number going off the screen. You can help me fix it if you want.
--- numberGoUp.js
+++ numberGoUp.js
@@ -2,7 +2,7 @@ time = 0
 test = 0
 timeup = 1
 
-totalTime = 180000
+totalTime = 163227
 document.getElementById("totaltime").innerHTML = "Total time: " + Math.floor(totalTime / 6000) + "m " + Math.ceil((totalTime / 100) % 60) + "s"
 
 function update() {
@@ -48,7 +48,7 @@ function timeToNumber(x) {
     return "10^^^^" + (1.002 ** (x - 128000) + 0.00004 * x + 0.219).toFixed(3)
   }
   //10^^^^100 < x < 10{5}5
-  else if (x < 180000) {
+  else if (x < 163227) {
     return "10^^^^" + timeToNumber((x - 129292) ** 1.15)
   }
   //10{5}5 < x
